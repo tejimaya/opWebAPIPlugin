0593c5ef05070ad5a9c202f1ace2673231995088
Kousuke Ebihara <ebihara@tejimaya.com>
2008-11-08 06:17:19 +0000

    #3111:added opWebAPIPlugin from Kansai open source 2008

---
+++ apps/feeds/modules/member/actions/actions.class.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * member actions.
+ *
+ * @package    OpenPNE
+ * @subpackage member
+ * @author     Kousuke Ebihara <ebihara@tejimaya.com>
+ */
+class memberActions extends sfActions
+{
+ /**
+  * Executes index action
+  *
+  * @param sfRequest $request A request object
+  */
+  public function executeIndex($request)
+  {
+  }
+}
