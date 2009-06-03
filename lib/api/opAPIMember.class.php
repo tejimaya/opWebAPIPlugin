79867060e84642cdd0a0ff27c6beb4e93a320c5e
Kousuke Ebihara <ebihara@tejimaya.com>
2009-06-03 20:48:44 +0900

    added the api for getting member entry

--- lib/api/opAPIMember.class.php
+++ lib/api/opAPIMember.class.php
@@ -44,7 +44,7 @@ class opAPIMember extends opAPI implements opAPIInterface
 
   public function entry()
   {
-    return false;
+    return $this->getRouteObject()->fetchOne();
   }
 
   public function insert(SimpleXMLElement $xml)
@@ -62,12 +62,33 @@ class opAPIMember extends opAPI implements opAPIInterface
     return false;
   }
 
+  public function generateEntryId($entry)
+  {
+    sfContext::getInstance()->getConfiguration()->loadHelpers(array('Url', 'opUtil'));
+
+    return app_url_for('pc_frontend', 'member/profile?id='.$entry->getId(), true);
+  }
+
   public function createEntryByInstance(Doctrine_Record $member, SimpleXMLElement $entry = null)
   {
     $entry = parent::createEntryByInstance($member, $entry);
     sfContext::getInstance()->getConfiguration()->loadHelpers(array('Url', 'opUtil'));
     $entry->setTitle($member->getName());
-    $entry->setLink(app_url_for('pc_frontend', 'member/profile?id='.$member->getId(), true), 'self', 'alternate');
+
+    $content = $entry->getElements()->addChild('content');
+    $content->addAttribute('type', 'xhtml');
+    $profiles = $content->addChild('div');
+    $profiles->addAttribute('xmlns', 'http://www.w3.org/1999/xhtml');
+
+    foreach ($member->getProfiles() as $profile)
+    {
+      $child = $profiles->addChild('div', $profile);
+      $child->addAttribute('id', $profile->getName());
+    }
+
+    $entry->setLink(url_for('@feeds_member_retrieve_resource_normal?model=member&id='.$member->getId()), 'self', 'application/atom+xml');
+    $entry->setLink(app_url_for('pc_frontend', 'member/profile?id='.$member->getId(), true), 'alternate', 'text/html');
+    $entry->setLink(app_url_for('mobile_frontend', 'member/profile?id='.$member->getId(), true), 'alternate');
 
     return $entry;
   }
