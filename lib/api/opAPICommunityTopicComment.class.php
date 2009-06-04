2834387a2b1468a71734934601dfb6e7d240d7fd
Kousuke Ebihara <ebihara@tejimaya.com>
2009-06-04 20:23:20 +0900

    changed the api for getting community topic comment feed for following GData more

--- lib/api/opAPICommunityTopicComment.class.php
+++ lib/api/opAPICommunityTopicComment.class.php
@@ -19,27 +19,25 @@ class opAPICommunityTopicComment extends opAPI implements opAPIInterface
 {
   public function feed()
   {
-    if ($id = $this->getParameter('topic_id'))
-    {
-      $communityTopicComment = Doctrine::getTable('CommunityTopicComment')->retrieveByCommunityTopicId($id);
-    }
-    else
-    {
-      $communityTopicComment = Doctrine::getTable('CommunityTopicComment')->doSelect(new Criteria());
-    }
+    $this
+      ->addConditionPublished()
+      ->addConditionUpdated()
+      ->setOrderBy()
+      ->setOffsetAndLimitation();
 
-    if (!$communityTopicComment)
+    $communityTopicComments = $this->getQuery()->execute();
+    if (!$communityTopicComments->count())
     {
       return false;
     }
 
-    $feed = $this->getGeneralFeed('CommunityTopicComments');
-    foreach ($communityTopicComment as $comment)
+    $feed = $this->getGeneralFeed($this->getParentObject()->getName(), $this->getTotalCount());
+    foreach ($communityTopicComments as $key => $comment)
     {
       $entry = $feed->addEntry();
       $this->createEntryByInstance($comment, $entry);
     }
-    $feed->setUpdated($communityTopicComment[0]->getCreatedAt());
+    $feed->setUpdated($communityTopicComments->getFirst()->getCreatedAt());
 
     return $feed->publish();
   }
@@ -90,10 +88,9 @@ class opAPICommunityTopicComment extends opAPI implements opAPIInterface
   {
     $entry = parent::createEntryByInstance($comment, $entry);
     sfContext::getInstance()->getConfiguration()->loadHelpers(array('Url', 'opUtil'));
-    $entry->setTitle($comment->getCommunityTopic()->getName());
     $entry->setContent($comment->getBody());
     $entry->setAuthorByMember($comment->getMember());
-    $entry->setLink(app_url_for('pc_frontend', 'communityTopic/detail?id='.$comment->getCommunityTopic()->getId(), true), 'self', 'alternate');
+    $entry->setLink(url_for('@feeds_community_topic_comment_retrieve_resource_normal?model=communityTopic&id='.$comment->getId()), 'self', 'application/atom+xml');
 
     return $entry;
   }
